9dca288e721a0b74d7bc61ba136bf2edb64f97ac
santhosh-575 <santhoshyedluri@75gmail.com>
2025-09-06 12:35:07 +0530

    thanks

--- practice.js
+++ practice.js
@@ -4,3 +4,15 @@ console.log(name)
 let num = 75;
 console.log(num)
 console.log(typeof(num))
+let x;
+console.log(typeof(x))
+let y = null;
+console.log(typeof(y))
+let arr = [1,2,3,4,5,6,7]
+console.log(arr)
+console.log(typeof(arr))
+let person = {
+    name : "santhosh",
+    id : 75
+}
+console.log(person.name)
